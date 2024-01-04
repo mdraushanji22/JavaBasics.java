ef1637e03fca388295360e99a2c76915cffc017f
mdraushanji22 <90023835+mdraushanji22@users.noreply.github.com>
2024-01-04 19:00:09 +0530

    How to take input in java

---
+++ src/JavaBasics.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class JavaBasics {
+public static void main (String[] args){
+    // How to take input in java
+    Scanner sc = new Scanner(System.in);
+
+    String name = sc.nextLine();
+
+    System.out.println(name);
+
+    int number = sc.nextInt();
+
+    System.out.println(number);
+
+    float ft = sc.nextFloat();
+
+    System.out.println(ft);
+
+    boolean bl = sc.nextBoolean();
+
+    System.out.println(bl);
+
+}
+}
